9ed3272ace58c76274e157814d0416b3c39ffc40
Mattia Dallapiccola <mattia.dallapiccola@eurac.edu>
2024-01-22 15:55:53 +0100

    Monthly, yearly, clearsky checks

--- on-the-fly_calculations/existing_PV_systems.py
+++ on-the-fly_calculations/existing_PV_systems.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def get_pv(lat, long, tilt, azimuth, peak_power):
+def get_pv(lat, long, tilt, azimuth, peak_power, years_period=None):
     '''
     Retrieves photovoltaic (PV) system data using the PVGIS API.
 
@@ -27,10 +27,14 @@ def get_pv(lat, long, tilt, azimuth, peak_power):
         photovoltaic power generation data based on the provided parameters.
         The returned data is in the form of a NumPy array.
     '''
+    if years_period == None:
+        years_period = 0
     losses = 14
-    year = 2019
+    year_end = 2020
+    year_start = year_end-years_period+1
+    
     url = 'https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=' + str(lat) + '&lon=' + str(
-        long) + '&startyear=' + str(year)+ '&endyear=' + str(year) + '&pvcalculation=' + str(1) + '&peakpower=' + str(peak_power) + '&loss=' + str(losses) + '&angle=' + str(
+        long) + '&startyear=' + str(year_start)+ '&endyear=' + str(year_end) + '&pvcalculation=' + str(1) + '&peakpower=' + str(peak_power) + '&loss=' + str(losses) + '&angle=' + str(
         tilt) + '&aspect=' + str(azimuth) + '&outputformat=csv&browser=1'
     print("Ready to get the PV data.")
     
@@ -51,9 +55,8 @@ def get_pv(lat, long, tilt, azimuth, peak_power):
 
         pv_weather_data = pd.read_csv(pv_filename, skiprows=pv_header_no_of_rows, skipfooter=pv_footer_no_of_rows,
                                       index_col=0, header=0, engine='python')
-                                      
-        pv_weather_data.index = pd.date_range('2019-01-01 00:00:00', '2019-12-31 23:00:00', freq='1H')
 
+        pv_weather_data.index = pd.date_range(f'{year_start}-01-01 00:00:00', f'{year_end}-12-31 23:00:00', freq='1H')
         #os.remove(pv_filename)
         print("PV data ready.")
         return pv_weather_data['P']
@@ -71,8 +74,9 @@ def get_pv(lat, long, tilt, azimuth, peak_power):
 
 def compare_energy_generation(coordinates, tilt, azimuth, nominal_power, real_energy_last_year):
     # Calculate estimated generation from satellite data
-    estimated_generation = get_pv(coordinates[0], coordinates[1], tilt, azimuth, nominal_power)
-    estimated_generation_sum = sum(estimated_generation)/1000
+    estimated_generation = get_pv(coordinates[0], coordinates[1], tilt, azimuth, nominal_power,years_period=5)
+    #estimated_generation_sum = sum(estimated_generation)/1000
+    estimated_generation_sum = estimated_generation.groupby(estimated_generation.index.year).sum().mean()/1000
     print("\nEstimated yearly energy generation: {} kWh.".format(round(estimated_generation_sum,1)))
     # Compare estimated generation with real energy generation from last year
     if real_energy_last_year < estimated_generation_sum*0.9  or real_energy_last_year > estimated_generation_sum*1.1:
@@ -83,7 +87,6 @@ def compare_energy_generation(coordinates, tilt, azimuth, nominal_power, real_en
         print("The system is performing well compared with expected data.")
     
     return estimated_generation
-
 
 # Function to generate suggestions based on the comparison
 def generate_suggestions(estimated_generation, real_energy_last_year):
@@ -120,14 +123,8 @@ def get_month_number(month_name):
         return None  # Return None for invalid month names
 
 
-
-
-
-
-
-# Example usage
-# Replace the arguments with actual data
-coordinates = (45.014, 11.357)
+# Input data
+coordinates = (46.457370, 11.322795)
 tilt = 30
 azimuth = 0    # deg (South=0, East=-90, 180=North, 90=West) 
 nominal_power = 1    # kWp
@@ -135,8 +132,32 @@ real_energy_last_year = 1230.3  # kWh
 
 PV_generation_profile = compare_energy_generation(coordinates, tilt, azimuth, nominal_power, real_energy_last_year)
 
+# plot yearly averages within the period
+average_yearly_values = PV_generation_profile.resample('Y').sum()/1000
+average_yearly_values.index = average_yearly_values.index.year
+average_yearly_values.plot.bar()   
+plt.title('Yearly cumulatives of a reference system')
+plt.ylabel('Energy Production [kWh]')
+plt.show()
+
+
+
+
+# plot monthly averages within the period
+average_monthly_values = PV_generation_profile.groupby(PV_generation_profile.index.month).mean()
+average_monthly_values.plot.bar()   
+average_monthly_values.sum()
+plt.title('Montly means')
+plt.xlabel('Month')
+plt.ylabel('Energy Production kWh')
+plt.show()
+
+
+
+
 print("\nLet's proceed with an additional test: monthly clearsky day comparison.\n")
 PV_generation_daily_sum = PV_generation_profile.resample('D').sum()/1000
+PV_generation_monthly_sum = PV_generation_profile.resample('M').sum()/1000
 
 def find_highest_energy_day(energy_data):
     # Initialize a dictionary to store the highest energy day for each month
@@ -149,22 +170,48 @@ def find_highest_energy_day(energy_data):
         # Store the result in the dictionary
         highest_energy_days[month] = {
             'day': highest_day.day,
+            'year': highest_day.year,
             'energy': daily_energy[highest_day]
         }
         
-
     return highest_energy_days
+
+def get_season_color(month):
+    winter_months = [12,1,2]
+    spring_months = [3,4,5]
+    summer_months = [6,7,8]
+    autumn_months = [9,10,11]
+    
+    
+    # Determine the season based on the month
+    if month in winter_months:
+        return 'blue'
+    elif month in spring_months:
+        return 'green'
+    elif month in summer_months:
+        return 'yellow'
+    elif month in autumn_months:
+        return 'orange'
+    else:
+        return None  # Return None for invalid month names
 
 # Call the function to find the highest energy day for each month
 result = find_highest_energy_day(PV_generation_daily_sum)
+
+
+
+
+
 
 # Plot only the highest energy day for each month
 plot_title = 'Hourly energy production profiles for highest energy days'
 plt.figure(figsize=(12, 6))
 for month, data in result.items():
-    highest_day_index = (PV_generation_profile.index.month == month) & (PV_generation_profile.index.day == data['day'])
+    color = get_season_color(month)
+
+    highest_day_index = (PV_generation_profile.index.year == data['year']) & (PV_generation_profile.index.month == month) & (PV_generation_profile.index.day == data['day'])
     highest_day_profile = PV_generation_profile[highest_day_index]
-    plt.plot(highest_day_profile.index.hour, highest_day_profile.values, label=f"Month {month}, Day {data['day']}")
+    plt.plot(highest_day_profile.index.hour, highest_day_profile.values, label=f"Year {data['year']}, Month {month}, Day {data['day']}", color=color)
 
 plt.title(plot_title)
 plt.xlabel('Hour of Day')
@@ -175,15 +222,21 @@ plt.show()
 print("1) Select the month you want to use for comparison.")
 month_name = 'February'
 month = get_month_number(month_name)
-print("Selected month is {}".format(month_name))
+print("Selected month is {}\n".format(month_name))
 day = result[month]['day']
-print("2) For the selected month, highest energy generation is {} kWh\n".format(result[month]['energy']))
+year = result[month]['year']
+print("2) For the selected month, highest expected energy generation is {} kWh\n".format(round(result[month]['energy'],1)))
 
-plot_title = 'Hourly energy production profiles for highest energy day of the selected month'
+plot_title = f'Hourly energy production profiles for {day} of {month_name} (highest energy day)'
 plt.figure(figsize=(12, 6))
-highest_day_index = (PV_generation_profile.index.month == month) & (PV_generation_profile.index.day == data['day'])
+highest_day_index = (PV_generation_profile.index.year == year) & (PV_generation_profile.index.month == month) & (PV_generation_profile.index.day == day)
 highest_day_profile = PV_generation_profile[highest_day_index]
-plt.plot(highest_day_profile.index.hour, highest_day_profile.values, label=f"Month {month}, Day {data['day']}")
+plt.plot(highest_day_profile.index.hour, highest_day_profile.values, label=f"Year {year}, Month {month}, Day {day}")
+plt.title(plot_title)
+plt.xlabel('Hour of Day')
+plt.ylabel('Energy Production [Wh]')
+plt.legend()
+plt.show()
 
 print("3) From your PV system monitoring app compare the daily generation and the shape of the generation profile.\n")
 print("4) Are them similar to the ones shown?\n")
@@ -203,7 +256,6 @@ elif answer == False:
     print("    - inverter failure")    # show example hourly generation curve
 
 
-    
 
 
 
