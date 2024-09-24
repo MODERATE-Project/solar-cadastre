1a0fc94fe1f3342a225fc37b440f0284c33bc111
UBIKGSPAIN <149668244+UBIKGSPAIN@users.noreply.github.com>
2024-09-24 10:22:22 +0200

    Delete front-moderate-main directory

--- front-moderate-main/src/app/directives/show-calculations.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowCalculationsDirective } from './show-calculations.directive';
-
-describe('ShowCalculationsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowCalculationsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- front-moderate-main/src/app/directives/show-tables.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowTablesDirective } from './show-tables.directive';
-
-describe('ShowTablesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowTablesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
